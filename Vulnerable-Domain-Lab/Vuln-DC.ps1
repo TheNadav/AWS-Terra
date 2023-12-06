283c0da0efcc7914da9402217ee4b0ce7f50f3dc
Nadav <105583152+TheNadav@users.noreply.github.com>
2023-12-06 11:54:53 +0200

    Update Vuln-DC.ps1

--- Vulnerable-Domain-Lab/Vuln-DC.ps1
+++ Vulnerable-Domain-Lab/Vuln-DC.ps1
@@ -1,3 +1,5 @@
+Add-WindowsFeature AD-Domain-Services
+
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\\Windows\\NTDS" -DomainMode "7" -DomainName "NAME.local" -DomainNetbiosName "NAME" -ForestMode "7" -InstallDns:$true -LogPath "C:\\Windows\\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\\Windows\\SYSVOL" -Force:$true
 
 IEX((new-object net.webclient).downloadstring("https://raw.githubusercontent.com/WaterExecution/vulnerable-AD-plus/master/vulnadplus.ps1"));
