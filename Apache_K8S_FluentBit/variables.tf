f89fb775c4cded3d6123ed990401b0ac877543f1
Nadav <105583152+TheNadav@users.noreply.github.com>
2024-01-01 21:41:34 +0200

    Add files via upload

---
+++ Apache_K8S_FluentBit/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+    default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
